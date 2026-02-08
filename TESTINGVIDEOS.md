1f8e6444c9d260284e7b54bf78d3aad3e9137366
oa14821cbsc <oa14821@carshaltonboys.org>
2026-02-08 20:30:03 +0000

    Update

--- TESTINGVIDEOS.md
+++ TESTINGVIDEOS.md
@@ -2,7 +2,8 @@
 
 ## Game Window
 
-### Test 001 - Failed Test
+### Test 001 - Successful Test
 
 https://github.com/oa14821cbsc/OCR-NEA-Computer-Science-A-Level-H446-03-IceTrap/assets/e930c573-afd3-4cf8-a505-cbfa0e19a31d
 
+### Test 002 - Successful Test
--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,33 @@ This module is the entrypoint to the program!
 """
 import pygame as pg
 
+
 class App:
     """
     This class represents the game window.
     """
-    pass
+
+    def __init__(self) -> None:
+        pg.init()
+        pass
+
+    def run() -> None:
+        pass
+
+    def update() -> None:
+        pass
+
+    def _handle_events() -> None:
+        pass
+
+
+def main() -> None:
+    """
+    Entrypoint of the program. This is where the app object is created and ran.
+    """
+    app = App()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
